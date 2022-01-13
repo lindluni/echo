ad9fc69273c7d5af57a1b348d224e21e5b13a3cf
Brett Logan <lindluni@github.com>
2022-01-13 10:55:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const core = require('@actions/core')
+const core = require('@actions/core');
 
 (async function main(){
     const message = core.getInput('message', {required: true, trimWhitespace: true})
